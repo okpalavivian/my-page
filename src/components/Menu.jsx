65e3f1a8067675a8a44431a6d0fede4fc38fbc82
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2023-03-05 13:25:46 +0000

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -30,7 +30,7 @@ const Menu = () => {
   return (
     <div className="menu">
       <h1>Other posts you may like</h1>
-      {posts?.map((post) => (
+      {posts.map((post) => (
         <div className="post" key={post.id}>
           <img src={post.img} alt="" />
           <h2>{post.title}</h2>
